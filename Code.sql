bc2bbf3393a0ec6203efd6f89901385c9e65d817
Saurabh Kumar <54509629+Saurabh2509@users.noreply.github.com>
2021-08-14 13:57:53 +0530

    Update Code.sql

--- Code.sql
+++ Code.sql
@@ -1,46 +1,196 @@
 PROBLEM_STATEMENT : 
 
 1.	Display details of jobs where the minimum salary is greater than 10000.
+
+        Select * from Hr.Jobs 
+        where MIN_SALARY>10000
+        
 2.	Display the first name and join date of the employees who joined between 2002 and 2005.
+
+        Select Hr.Employees.FIRST_NAME ,Hr.Job_history.START_DATE From Hr.Employees 
+        Join Hr.Job_history 
+        Using(JOB_ID) 
+        where START_DATE between Date'2002-01-01' and Date '2005-12-31'
+        
 3.	Display first name and join date of the employees who is either IT Programmer or Sales Man.
+
+        Select emp.FIRST_NAME,emp.hire_date,jbs.JOB_TITLE  From Hr.Employees emp
+        join Hr.jobs jbs
+        using(job_id) 
+        where JOB_TITLE='Programmer' OR JOB_TITLE='Sales Manager' 
+        
 4.	Display employees who joined after 1st January 2008.
+
+        Select Hr.Employees.EMPLOYEE_ID,Hr.Employees.FIRST_NAME,Hr.Employees.LAST_NAME,Hr.Job_history.START_DATE  From Hr.Employees 
+        Join Hr.Job_history 
+        Using(job_id) 
+        where START_DATE  > Date '2008-01-01' 
+        
 5.	Display details of employee with ID 150 or 160.
+
+        Select Hr.Employees.EMPLOYEE_ID,Hr.Employees.FIRST_NAME,Hr.Employees.LAST_NAME From  Hr.Employees 
+        where EMPLOYEE_ID=150 OR EMPLOYEE_ID=160
+
 6.	Display first name, salary, commission pct, and hire date for employees with salary less than 10000.
+
+        Select Hr.Employees.FIRST_NAME,Hr.Employees.SALARY,Hr.Employees.HIRE_DATE ,Hr.Employees.COMMISSION_PCT From  Hr.Employees 
+        where SALARY<10000
+        
 7.	Display job Title, the difference between minimum and maximum salaries for jobs with max salary in the range 10000 to 20000.
+
+        Select Hr.Jobs.JOB_TITLE ,Hr.Jobs.MAX_SALARY-Hr.Jobs.MIN_SALARY as diff_sal  From Hr.Jobs 
+        where Hr.Jobs.MAX_SALARY-Hr.Jobs.MIN_SALARY between 10000 and 20000 
+        
 8.	Display first name, salary, and round the salary to thousands.
+
+        Select Hr.Employees.FIRST_NAME,round(Hr.Employees.SALARY,-3) From Hr.Employees
+
 9.	Display details of jobs in the descending order of the title.
+
+        Select * from Hr.jobs 
+        order by JOB_TITLE  Desc
+
 10.	Display employees where the first name or last name starts with S.
+
+        Select Hr.Employees.*   From Hr.Employees 
+        where Hr.Employees.FIRST_NAME like 's%' or  Hr.Employees.LAST_NAME like '%s' 
+        
 11.	Display employees who joined in the month of May.
+
+        Select Hr.Employees.*   From Hr.Employees 
+        where to_char(HIRE_DATE,'mon')='may'
+        
 12.	Display details of the employees where commission percentage is null and salary in the range 5000 to 10000 and department is 30.
+
+        Select Hr.Employees.*   From Hr.Employees 
+        where COMMISSION_PCT is null and SALARY between 5000 and 10000 and DEPARTMENT_ID=60  
+        
 13.	Display first name and date of first salary of the employees.
+
+        Select Hr.Employees.FIRST_NAME ,LAST_DAY(HIRE_DATE)+1 as Date_of_first_salary From Hr.Employees 
+        ----[or]
+        Select Hr.Employees.FIRST_NAME ,LAST_DAY(HIRE_DATE) as Date_of_first_salary From Hr.Employees 
+        
 14.	Display first name and experience of the employees.
+
+        Select emp.FIRST_NAME,FLOOR((SYSDATE-emp.HIRE_DATE)/365) as Exprience From Hr.Employees emp
+
 15.	Display first name of employees who joined in 2001.
+
+        Select emp.FIRST_NAME,emp.HIRE_DATE  From Hr.Employees emp
+        where Hire_Date between Date'2001-01-01' and Date '2001-12-31' 
+
 16.	Display first name and last name after converting the first letter of each name to upper case and the rest to lower case.
+
+        Select INITCAP(Hr.Employees.FIRST_NAME) as First_Name ,INITCAP(Hr.Employees.LAST_NAME) as Last_Name From Hr.Employees 
+
 17.	Display the first word in job title.
+
+        Select SUBSTR(Hr.jobs.job_title,1) From Hr.jobs 
+
 18.	Display the length of first name for employees where last name contain character ‘b’ after 3rd position.
+
+
 19.	Display first name in upper case and email address in lower case for employees where the first name and email address are same irrespective of the case.
+
+        Select Upper(Hr.Employees.FIRST_NAME) ,lower(Hr.Employees.EMAIL) From Hr.Employees 
+        where FIRST_NAME = EMAIL
+
 20.	Display employees who joined in the current year.
+
+        Select Hr.Employees.*  From Hr.Employees  
+        where to_char(HIRE_DATE,'YYYY') =to_char(SYSDATE,'YYYY')
+        
 21.	Display the number of days between system date and 1st January 2011.
+        
+        Select Sysdate-Date'2011-01-01' From Dual
+        
 22.	Display how many employees joined in each month of the current year.
+
+        Select to_char(Hr.Employees.HIRE_DATE,'MM') ,count(*) From  Hr.Employees 
+        where to_char(HIRE_DATE,'YYYY') = to_char(SYSDATE,'YYYY')  
+        Group by to_char(HIRE_DATE,'MM')
+        
 23.	Display manager ID and number of employees managed by the manager.
+
+        Select emp.MANAGER_ID,count(emp.employee_id) From  Hr.Employees emp
+        Group by MANAGER_ID	
+        
 24.	Display employee ID and the date on which he ended his previous job.
 25.	Display number of employees joined after 15th of the month.
+
+        Select FIRST_NAME,LAST_NAME,HIRE_DATE From Hr.Employees 
+        where to_char(HIRE_DATE ,'dd') >'15'
+
 26.	Display the country ID and number of cities we have in the country.
+
+        Select COUNTRY_ID,Count(CITY) From Hr.Locations  
+        Group by COUNTRY_ID
+        
 27.	Display average salary of employees in each department who have commission percentage.
-28.	Display job ID, number of employees, sum of salary, and difference between highest salary and lowest salary of the employees of the job.[Discuss]
+
+        Select Avg(SALARY) ,DEPARTMENT_ID From Hr.Employees 
+        where COMMISSION_PCT >0 
+        Group by DEPARTMENT_ID
+        
+28.	Display job ID, number of employees, sum of salary, and difference between highest salary and lowest salary of the employees of the job.
+
+        Select Job_id ,Sum(Salary),Max(Salary)-Min(Salary),count(EMPLOYEE_ID) From Hr.Employees
+        Group by job_id
+        
 29.	Display job ID for jobs with average salary more than 10000.
+
+        Select Job_id ,avg(salary) From Hr.Employees
+        Group by job_id having avg(Salary) >10000
+        
 30.	Display years in which more than 10 employees joined.
+
+        Select to_char(Hire_date,'YYYY') From Hr.Employees 
+        Group by to_char(Hire_date,'YYYY') having count(EMPLOYEE_ID)>10
+        
 31.	Display departments in which more than five employees have commission percentage.
+
+        Select DEPARTMENT_ID  From Hr.Employees  
+        where COMMISSION_PCT>0 
+        Group by DEPARTMENT_ID  having count(EMPLOYEE_ID)>5 
+        
 32.	Display employee ID for employees who did more than one job in the past.
+
+        Select EMPLOYEE_ID from Hr.Job_history  
+        Group by EMPLOYEE_ID having count(Employee_id)>1
+        
 33.	Display job ID of jobs that were done by more than 3 employees for more than 100 days.
+
+        Select Job_ID From Hr.Job_history 
+        where (End_date-start_date)>100
+        Group by Job_id having count(Employee_id)>3
+        
 34.	Display department ID, year, and Number of employees joined.
+
+        Select Department_id ,to_char(hire_date,'YYYY'),count(Employee_id)  From hr.Employees
+        Group by DEPARTMENT_ID, (HIRE_DATE, 'YYYY') 
+        order by DEPARTMENT_ID
+        
 35.	Display departments where any manager is managing more than 5 employees.
+
+        Select Department_id  From hr.Employees 
+        Group by Department_id having  count(MANAGER_ID)>5
+        
 36.	Change salary of employee 115 to 8000 if the existing salary is less than 6000.
-        [ORA-01031: insufficient privileges]
+
+        Update Hr.Employees  
+        Set Salary=8000 
+        where employee_id =115 and Salary <6000 
+        Error:[ORA-01031: insufficient privileges]
+        
 38.	Insert a new employee into employees with all the required details.
-        [ORA-01031: insufficient privileges]
+
+        Error:[ORA-01031: insufficient privileges]
+        
 40.	Change job ID of employee 110 to IT_PROG if the employee belongs to department 10 and the existing job ID does not start with IT.
-        [ORA-01031: insufficient privileges]
+
+        Error:[ORA-01031: insufficient privileges]
+        
 41.	Display job title, department name, employee last name, starting date for all jobs from 2000 to 2005.
 
         Select 	jbl.JOB_TITLE,dept.DEPARTMENT_NAME,emp.LAST_NAME,jbh.START_DATE From Hr.Employees emp 
